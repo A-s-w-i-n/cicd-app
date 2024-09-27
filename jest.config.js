ba009d725491937da10a1034a005dfddafba21e4
aswin <aswin@kssmart.co>
2024-09-27 12:51:20 +0530

    change

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.js$': 'babel-jest', // This is necessary if you are using Babel
-    },
-    moduleFileExtensions: ['js'], // Recognize .js file extensions
-  };
